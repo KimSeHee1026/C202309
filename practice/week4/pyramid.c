af592b3e54aa0b42768279052900c52c8451dae4
KimSeHee1026 <ksh184556@gmail.com>
2023-09-25 16:15:27 +0900

    Week4.1 반목문 실습 + 피라미드 쌓기

--- practice/week4/pyramid.c
+++ practice/week4/pyramid.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 
 int main(void) {
-	//floor 함수 초기화
-	int floor;
+	//user_floor 함수 초기화
+	int user_floor;
+
 	// 사용자에게 몇 층을 쌓을지 입력값을 받는 함수
 	printf("몇 층을 쌓겠습니까? (5 ~ 100)");
-	scanf_s("%d", &floor);
+	scanf_s("%d", &user_floor);
 
-	for (int i = 0; i < floor; i++) // 사용자에게 받은 입력값만큼 줄을 출력하는 함수
-	{
-		for (int j = 0; j < floor - 1 - i; j++) // 사용자에게 받은 입력값을 floor 줄애 -1-i 를 계산 후 빈칸을 출력하는 함수
-		{
-			printf("S");
+	// 사용자의 입력값이 5와 100사이여야 하는 조건문을 설립
+	if (user_floor < 5 || user_floor > 100) {
+		printf("값을 잘못 입력하셨습니다. 5부터 100사이의 값을 입력해주세요.");
+	} while (user_floor < 5 || user_floor > 100);
+
+	// 사용자에게 받은 입력값만큼 줄을 출력하는 함수
+		for (int i = 0; i < user_floor; i++) {
+
+			// 사용자에게 받은 입력값을 floor 줄애 -1-i 를 계산 후 빈칸을 출력하는 함수
+			for (int j = 0; j < user_floor - 1 - i; j++) {
+				printf("S");
+			}
+
+			// *를 해당 줄 중 *2+1로 홀수로 출력하는 함수
+			for (int k = 0; k <= i * 2 + 1; k++) {
+				printf("*");
+			}
+
+			printf("\n");
 		}
-		for (int k = 0; k <= i * 2 + 1; k++) // *를 해당 줄 중 *2+1로 홀수로 출력하는 함수
-		{
-			printf("*");
-		}
-		printf("\n");
+
+		return 0;
 	}
-	return 0;
-}
